32eccb695763f0e54094f380e05291a718255def
caden <topfloorbosscaden@gmail.com>
2023-12-29 05:33:32 -0600

    Update packet_sniffer.c

--- packet_sniffer.c
+++ packet_sniffer.c
@@ -52,9 +52,11 @@ void packet_handler(unsigned char *user_data, const struct pcap_pkthdr *pkthdr, 
 
     current->count++;
 
-    printf("\rPackets from %s: %u    ", source_ip, current->count);
-    fflush(stdout);
+    // Print console output without timestamps and additional information
+    printf("\rPackets captured: %u", current->count);
+    fflush(stdout);  // Flush the console output to ensure it's updated immediately
 
+    // Update output file with previous information
     time_t timestamp = pkthdr->ts.tv_sec;
     fprintf(output_file, "[%s] Packet captured, size: %d bytes\n", ctime(&timestamp), pkthdr->len);
     fprintf(output_file, "    Source IP: %s\n", source_ip);
@@ -65,6 +67,7 @@ void packet_handler(unsigned char *user_data, const struct pcap_pkthdr *pkthdr, 
     fprintf(output_file, "\n");
     fprintf(output_file, "    Condensed information: Packets from %s: %u\n\n", source_ip, current->count);
 
+    // Avoid "double free" issue
     if (packet_counts != NULL) {
         struct PacketCount *temp = packet_counts;
         packet_counts = packet_counts->next;
