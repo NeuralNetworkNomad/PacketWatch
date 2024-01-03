ee30946075d871ec5c67673e18df286051fb277d
caden <topfloorbosscaden@gmail.com>
2024-01-02 21:49:45 -0600

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,20 @@
 
 #include "packet_sniffer.h"
 
-int main() {
-    // Start packet capture on interface "eth0," write to "packet_capture.txt," capture 10 packets, and filter for TCP
-    start_packet_capture("eth0", "packet_capture.txt", 100, "tcp");
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s <interface> <output_file> <packet_count> <filter_expression>\n", argv[0]);
+        return 1;
+    }
+
+    // Print the command-line arguments
+    printf("Interface: %s\n", argv[1]);
+    printf("Output File: %s\n", argv[2]);
+    printf("Packet Count: %s\n", argv[3]);
+    printf("Filter Expression: %s\n", argv[4]);
+
+    // Start packet capture
+    start_packet_capture(argv[1], argv[2], atoi(argv[3]), argv[4]);
     stop_packet_capture();
 
     return 0;
